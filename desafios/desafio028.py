b36c715b222a5b8e2e5cd0887fdb92920481c27f
viniciusaze <152533810+viniciusaze@users.noreply.github.com>
2024-01-31 11:25:09 -0300

    melhorado desafio 28

--- desafios/desafio028.py
+++ desafios/desafio028.py
@@ -1,9 +1,17 @@
 from random import randint
-certo = randint(0, 5)
-escolha = int(input('Escolha um número entre 0 e 5: '))
+from time import sleep
+certo = randint(0, 5) # Randomiza o número
+print('*~*~' * 15)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar')
+print('*~*~' * 15)
+
+escolha = int(input('Em que número eu pensei? ')) # Jogador escolhe o número
+
 print('Será que você acertou?')
+sleep(2) # Deixa um tempo de 2s para o proximo passo
+
 if certo == escolha:
     print(f'Parabéns!!! Acertou o número {certo}')
 else:
-    print(f'Que pena! Você errou... o número sorteado foi {certo}')
+    print(f'Não foi dessa vez! O número certo era {certo}, você escolheu o {escolha}')
 print('Fim do jogo')
