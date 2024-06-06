386bd36e45e5c274bd23ba3d7f2fd09f5286288c
viniciusaze <152533810+viniciusaze@users.noreply.github.com>
2024-06-06 18:30:39 -0300

    update sb

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,11 +1,16 @@
+from time import sleep
+import os
+
 saldo = 0
 limite = 500
 extrato = ''
 numero_saques = 0
-limite_saques = 3
+LIMITE_SAQUES = 3
 
 
 def Menu():
+    os.system('cls')
+
     opcao = input(''' Sistema Bancário
           
 Selecione a opção desejada:
@@ -15,20 +20,81 @@ Selecione a opção desejada:
 [e] Extrato
 [q] Sair
             
--->''').lower().strip()
+--> ''').lower().strip()
     
     while True:
 
         if opcao == 'd':
-            print('deposito')
-            break
+            Deposito()
         elif opcao == 's':
-            print('saque')
-            break
+            Saque()
         elif opcao == 'e':
-            print('extrato')
-            break
+            Extrato()
         elif opcao == 'q':
             break
 
+
+def Deposito():
+    os.system('cls')
+
+    global saldo, extrato
+
+    deposito = float(input('''
+DEPÓSITO
+Valor a depositar: R$ '''))
+    
+    saldo += deposito
+
+    extrato += f'Depósito - R$ {deposito}\n'
+
+    Menu()
+
+def Saque():
+    os.system('cls')
+
+    global saldo, limite, numero_saques, LIMITE_SAQUES, extrato   
+
+    saque = float(input('''
+SAQUE
+Valor a sacar: R$ '''))
+    
+    while True:
+        if saque > 500:
+            os.system('cls')
+            print('Seu saque excede o limite de 500.')
+            sleep(5)
+            Saque()
+        else:
+            break
+    
+    if saque > saldo:
+        os.system('cls')
+        print('Você não possui saldo suficiente para esta transação')
+        sleep(5)
+        Saque()
+    
+    numero_saques += 1
+
+    if numero_saques > LIMITE_SAQUES:
+        os.system('cls')
+        print('Você atingiu o limite de saques')
+        sleep(5)
+        Menu()
+
+    saldo -= saque
+
+    extrato += f'Saque - R$ {saque}\n'
+
+    Menu()
+    
+def Extrato():
+    global extrato, saldo
+    print(f'EXTRATO\n{extrato} Saldo Total: {saldo}')
+
+    sair = input('Aperte uma tecla para sair ')
+    Menu()
+    
+
 Menu()
+
+
