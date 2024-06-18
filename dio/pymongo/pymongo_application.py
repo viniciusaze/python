8cc2c826e3ee3d8523ee2c7a9f278d456225fa65
viniciusaze <152533810+viniciusaze@users.noreply.github.com>
2024-06-18 16:26:13 -0300

    pymongo

--- dio/pymongo/pymongo_application.py
+++ dio/pymongo/pymongo_application.py
@@ -1,8 +1,12 @@
 import datetime
+import pprint
+
 import pymongo as pyM
 
+password = input("Digite a senha: ")
+
 # Conectando
-client = pyM.MongoClient("mongodb+srv://ar-vinicius:<password>@cluster0.cx1y4t0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = pyM.MongoClient(f"mongodb+srv://ar-vinicius:{password}@cluster0.cx1y4t0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 db = client.test
 collection = db.test_collection
@@ -18,6 +22,34 @@ post = {
 
 # Preparando para submeter as infos
 posts = db.posts
-
 post_id = posts.insert_one(post).inserted_id
 print(post_id)
+
+# print(db.posts.find_one())
+# Mostra a estrutura de forma indentada
+pprint.pprint(db.posts.find_one())
+
+# Criando outro documento
+# bulk inserts
+new_posts = [{
+    "author": "Mike",
+    "text": "Another post",
+    "tags": ["bulk", "post", "insert"],
+    "date": datetime.datetime.now()
+    },
+    {
+    "author": "Joao",
+    "text": "Post from Joao. New post available",
+    "title": "Mongo is fun",
+    "date": datetime.datetime(2009,11,10,10,45)
+    }]
+
+result = posts.insert_many(new_posts)
+print(result.inserted_ids)
+
+print("\nRecuperação Final")
+pprint.pprint(db.posts.find_one({"author": "Joao"}))
+
+print("\nDocumentos presentes na coleção posts")
+for post in posts.find():
+    pprint.pprint(post)
---
+++ dio/pymongo/second_file_pymongo.py
@@ -0,0 +1,42 @@
+import datetime
+import pprint
+
+import pymongo
+import pymongo as pyM
+
+password = input("Digite a senha: ")
+
+# Conectando
+client = pyM.MongoClient(f"mongodb+srv://ar-vinicius:{password}@cluster0.cx1y4t0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+db = client.test
+posts = db.posts
+
+for post in posts.find():
+    pprint.pprint(post)
+
+print(posts.count_documents({}))
+print(posts.count_documents({"author": "Mike"}))
+print(posts.count_documents({"tags": "insert"}))
+
+print("\nRecuperando info da coleção post de maneira ordenada")
+for post in posts.find({}).sort("date"):
+    pprint.pprint(post)
+
+# Criando indices
+result = db.profiles.create_index([('author', pymongo.ASCENDING)],
+                                  unique=True)
+
+print(sorted(list(db.profiles.index_information())))
+
+# Criando profiles
+user_profile_user = [
+    {'user_id': 211, 'name': 'Luke'},
+    {'user_id': 212, 'name': 'Joao'}]
+
+result = db.profile_user.insert_many(user_profile_user)
+
+print("\nColeções armazenadas no mongodb")
+collections = db.list_collection_names()
+for collection in collections:
+    print(collection)
