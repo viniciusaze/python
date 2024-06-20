c1baee02093de72adf6e77c1ffd583deb31b378a
viniciusaze <152533810+viniciusaze@users.noreply.github.com>
2024-06-20 16:43:18 -0300

    update challenge

--- projetos/projetos_integrando_python_com_SQLite_MongoDB/main.py
+++ projetos/projetos_integrando_python_com_SQLite_MongoDB/main.py
@@ -16,7 +16,9 @@ from sqlalchemy import select
 
 Base = declarative_base()
 
+
 class Client(Base):
+    # Create Table and attributes of Client
     __tablename__ = "client"
 
     id = Column(Integer, primary_key=True)
@@ -24,8 +26,12 @@ class Client(Base):
     cpf = Column(String(9))
     address = Column(String(50))
 
+    # Create relationship
+    account = relationship("Account", back_populates="client")
+
 
 class Account(Base):
+    # Create Table and attributes of Account
     __tablename__ = "account"
 
     id = Column(Integer, primary_key=True)
@@ -34,3 +40,14 @@ class Account(Base):
     number_account = Column(Integer)
     id_client = Column(Integer, ForeignKey("client.id"), nullable=False)
     balance = Column(DECIMAL)
+
+    # Create relationship
+    client = relationship("Client", back_populates="account")
+
+
+# Connecting with database
+engine = create_engine("sqlite://")
+
+# Create tables with class
+Base.metadata.create_all(engine)
+
