8aca5bd118f988a5d19db7d19d22ae26117f56e2
viniciusaze <152533810+viniciusaze@users.noreply.github.com>
2024-06-20 16:51:54 -0300

    update challenge

--- projetos/projetos_integrando_python_com_SQLite_MongoDB/main.py
+++ projetos/projetos_integrando_python_com_SQLite_MongoDB/main.py
@@ -23,7 +23,7 @@ class Client(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(30))
-    cpf = Column(String(9))
+    cpf = Column(String(11))
     address = Column(String(50))
 
     # Create relationship
@@ -36,7 +36,7 @@ class Account(Base):
 
     id = Column(Integer, primary_key=True)
     type = Column(String(30))
-    agency = Column(String(10))
+    agency = Column(String(8))
     number_account = Column(Integer)
     id_client = Column(Integer, ForeignKey("client.id"), nullable=False)
     balance = Column(DECIMAL)
@@ -50,4 +50,29 @@ engine = create_engine("sqlite://")
 
 # Create tables with class
 Base.metadata.create_all(engine)
+
+with Session(engine) as session:
+    client_1 = Client(
+        name="Vinícius Azevedo Rosso",
+        cpf="03643545002",
+        address="Rio Grande do Sul",
+        account=[Account(
+            type="Conta corrente",
+            agency="00216505",
+            number_account=886312,
+            balance=567.98
+        )]
+    )
+
+    client_2 = Client(
+        name="Gabriel Teixeira Fagundes",
+        cpf="064874650",
+        address="Paraná"
+    )
+
+    client_3 = Client(
+        name="Camila da Silva",
+        cpf="874320654",
+        address="Paraná"
+    )
 
